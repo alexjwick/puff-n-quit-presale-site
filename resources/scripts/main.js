e57f66813b48e81524ae078d42a01e14cb27aa9e
alexjwick <alexjwick03@gmail.com>
2023-06-24 00:50:33 -0400

    huge progress, installed webpack, setup with firebase

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,34 +1,102 @@
-window.onload = function () {
-    var form = document.getElementById("emailForm");
-    if (!form) {
-        console.error("Could not find element with ID 'emailForm'");
-        return;
-    }
-    if (!(form instanceof HTMLFormElement)) {
-        console.error("Element with ID 'emailForm' is not a form element");
-        return;
-    }
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        handleFormSubmit(form);
-    });
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkhSb61yqK8Q2BaVUAV5ShYsThvrt_cvc", // This is a public key
+  authDomain: "puff--n-quit-presale-site.firebaseapp.com",
+  projectId: "puff--n-quit-presale-site",
+  storageBucket: "puff--n-quit-presale-site.appspot.com",
+  messagingSenderId: "1045456087759",
+  appId: "1:1045456087759:web:a2c7e85004e7bf5a4025ba",
+  measurementId: "G-9RGR65MTVS",
 };
-function handleFormSubmit(form) {
-    var emailInput = form.elements.namedItem("email");
-    if (!emailInput) {
-        console.error("Could not find input element with name 'email' in form");
-        return;
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.onload = () => {
+  console.log("Page loaded.");
+  // Get email form
+  const emailForm = document.querySelector(".email-form");
+
+  // Check if email form exists
+  if (!emailForm) {
+    return console.error("Form with class 'email-form' not found.");
+  }
+
+  // Get email input field
+  const emailInput = emailForm.querySelector(".email-input");
+
+  // Check if email input field exists
+  if (!emailInput) {
+    return console.error("Input with class 'email-input' not found.");
+  }
+
+  // Get submit button
+  const submitButton = emailForm.querySelector(".submit-button");
+
+  // Check if submit button exists
+  if (!submitButton) {
+    return console.error("Button with class 'submit-button' not found.");
+  }
+
+  console.log("Email form found.");
+
+  // Add event listener for submit button
+  submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    handleEmailFormSubmit(emailInput);
+  });
+
+  emailInput.addEventListener("keypress", (event) => {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleEmailFormSubmit(emailInput);
     }
-    if (!(emailInput instanceof HTMLInputElement)) {
-        console.error("Element with name 'email' in form is not an input element");
-        return;
-    }
-    var email = emailInput.value;
-    if (!email) {
-        console.error("Email input is empty");
-        return;
-    }
-    console.log("Email submitted: ".concat(email));
-    // Send email code to backend here
+  });
+};
+
+/**
+ * Handles the email form submit event
+ * @returns void
+ */
+function handleEmailFormSubmit(emailInput) {
+  // Validate email
+  const email = emailInput.value.trim();
+  if (!email) {
+    return;
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return console.error("Invalid email entered."); //TODO: Display error message to user
+  }
+
+  emailInput.value = "";
+  console.log("Collected email:", email);
+  addEmailToDatabase(email);
 }
-//# sourceMappingURL=main.js.map
+
+/**
+ * Adds an email to the database
+ * @param email The email to add
+ * @returns void
+ */
+async function addEmailToDatabase(email) {
+  try {
+    const docRef = await addDoc(collection(db, "emails"), {
+      email: email,
+    });
+
+    if (!docRef) {
+      console.error("Document reference is undefined");
+      return;
+    }
+
+    console.log("Added email to database: ", email);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+}
